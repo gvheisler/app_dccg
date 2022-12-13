246efefc1955124a3adcda55045a9b54f0d3564e
gvheisler <gvheisler@inf.ufsm.br>
2022-12-13 19:23:32 -0300

    tela inicial melhorada

---
+++ lib/main_menu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class mainMenu extends StatefulWidget {
+  const mainMenu({Key? key}) : super(key: key);
+
+  @override
+  State<mainMenu> createState() => _mainMenuState();
+}
+
+class _mainMenuState extends State<mainMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
