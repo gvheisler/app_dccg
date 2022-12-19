f2e7ecce34a9c5787f304e18e1193500d6bef7c1
gvheisler <gvheisler@inf.ufsm.br>
2022-12-18 23:13:46 -0300

    mais paginas

--- lib/pages/htp_page.dart
+++ lib/pages/htp_page.dart
@@ -9,7 +9,7 @@ class HtpPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Como Jogar', style: TextStyle(fontSize: 30, color: Colors.black),),
       ),
-      body: Center(
+      body: const Center(
           child: Text('Pellentesque dignissim nisl quis ante fringilla, eget mattis neque feugiat. Suspendisse pretium nec dolor nec aliquet. Aliquam nec magna in arcu eleifend pellentesque et congue mauris. In sapien odio, fermentum id diam a, sollicitudin rutrum quam. Nunc massa elit, vestibulum at urna ut, lobortis ornare ex. Quisque id lectus dictum, tempor est vel, congue mauris. Nulla pharetra consectetur dolor id venenatis. Proin aliquet cursus libero vitae varius. Quisque at luctus elit, fermentum sagittis enim.', textAlign: TextAlign.center, style: TextStyle(fontSize: 20),)),
     );
   }
