713d404297277935d5a52680acb635107a9b787b
gvheisler <gvheisler@inf.ufsm.br>
2022-12-22 12:15:03 -0300

    tela de batalha

--- lib/pages/battle_page.dart
+++ lib/pages/battle_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_dccg/misc/buttons.dart';
 import 'package:flutter/material.dart';
 
 class BattlePage extends StatelessWidget {
@@ -5,6 +6,42 @@ class BattlePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+      title: Text('Escolha um inimigo para batalhar!'),
+      ),
+      body: Center(
+        child: Container(
+          color: Colors.grey,
+          height: 505,
+          width: 405,
+          child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              color: Colors.white,
+              height: 500,
+              width: 400,
+              child: ListView(
+                children: [
+                  DefaultButton(route: '/', text: 'text'),
+                  DefaultButton(route: '/', text: 'text'),
+                  DefaultButton(route: '/', text: 'text'),
+                  DefaultButton(route: '/', text: 'text'),
+                  DefaultButton(route: '/', text: 'text'),
+                  DefaultButton(route: '/', text: 'text'),
+                  DefaultButton(route: '/', text: 'text'),
+                  DefaultButton(route: '/', text: 'text'),
+                  DefaultButton(route: '/', text: 'text'),
+                  DefaultButton(route: '/', text: 'text'),
+                  DefaultButton(route: '/', text: 'text'),
+
+                ],
+
+              )
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
