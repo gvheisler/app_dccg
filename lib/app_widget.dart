3116c31284e3736eeb462738b548fa22f2e9ff8c
gvheisler <gvheisler@inf.ufsm.br>
2022-12-30 10:49:32 -0300

    organização

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,7 +1,7 @@
 import 'package:app_dccg/controller/app_controller.dart';
 import 'package:app_dccg/pages/battle_page.dart';
 import 'package:app_dccg/pages/book_page.dart';
-import 'package:app_dccg/pages/challenge_page.dart';
+import 'package:app_dccg/pages/game/challenge_page.dart';
 import 'package:app_dccg/pages/game_page.dart';
 import 'package:app_dccg/pages/help_page.dart';
 import 'package:app_dccg/pages/home_page.dart';
